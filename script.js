c69e4076f245eeac9d36c10d2c5bea129a5a1d29
akyadav4710 <163339150+akyadav4710@users.noreply.github.com>
2024-08-06 14:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,29 +8,19 @@ function convertToRoman(num) {
         5: ['V', 5], 
         6: ['I', 1]
     };
-	    const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
-    
+	     const values = 
+        [1000, 900, 500, 400, 100, 
+         90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = 
+        ['M', 'CM', 'D', 'CD', 'C', 
+         'XC', 'L', 'XL', 'X', 'IX', 
+         'V', 'IV', 'I'];
     let result = '';
 
-    for (let i = 0; i < romanSymbols.length; i++) {
-        const [symbol, value] = romanSymbols[i];
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
         }
     }
 
